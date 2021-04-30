a55939eea9de487c9dd6b6a72548b109bb88d9a0
Cezar Mocanu <cmocanu@cisco.com>
2021-04-30 14:57:23 +0300

    [master] Cleared missshapes

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,25 @@ import {ContactForm} from '../components/contact-form/ContactForm';
 export default function Home() {
   return (
     <div className="App">
-      
+      <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div className="container">
+                <a className="navbar-brand js-scroll-trigger" href="#page-top">SMART PART</a>
+                <button className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i className="fas fa-bars"></i>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Servicii</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">Despre Noi</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#etape">Demo Gratuit</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         
-        {/* <header className="masthead bg-primary text-white text-center">
+        <header className="masthead bg-primary text-white text-center">
             <div className="container d-flex align-items-center flex-column">
                 
                 <div className="icon-background mb-5">
@@ -237,7 +253,7 @@ export default function Home() {
                     </script>
                 </small>
             </div>
-        </div> */}
+        </div>
     </div>
   )
 }
