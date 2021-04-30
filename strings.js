7b33ccba7d23be56406db0544baf822afcd44ab0
Cezar Mocanu <cmocanu@cisco.com>
2021-04-30 23:12:21 +0300

    [FE0002] Refactored Header

--- strings.js
+++ strings.js
@@ -1,7 +1,14 @@
 const LABELS = {
     about: 'Despre Noi',
+    automate: 'Automatizează',
     contact: 'Contact',
+    digitalize: 'Digitalizează',
+    evolve: 'Evoluează',
+    freeDemo: 'Demo Gratuit',
+    getFreeDemo: 'Obține Demo Gratuit',
+    metaDescription: 'Obține o analiză și un demo gratuit! Digitalizează-ți afacerea și automatizează procesele repetitive și inutile printr-o aplicatie Smart Part.',
     smartPart: 'Smart Part',
+    smartPartCallToAction: 'Smart Part|Digitalizează-ți afacerea!',
     steps: 'Etape',
     services: 'Servicii'
 }
