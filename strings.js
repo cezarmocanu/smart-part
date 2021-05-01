88c5b03d8a4f3aed14a8b9747608007b20d95fc7
Cezar Mocanu <cmocanu@cisco.com>
2021-05-01 13:16:48 +0300

    [FE0003] Refactored portofolio component and added logo

--- strings.js
+++ strings.js
@@ -4,13 +4,23 @@ const LABELS = {
     contact: 'Contact',
     digitalize: 'Digitalizează',
     evolve: 'Evoluează',
+    feedbackBasedSolution: 'Soluție bazată pe feedback',
+    feedbackBasedSolutionDescription: 'Construim o soluție digitală bazată pe nevoile și provocarile pe care le întampinați.',
     freeDemo: 'Demo Gratuit',
+    forgetPapers: 'Uită de hârtii!',
+    forgetPapersDescription: 'Propunem soluții ce au rolul de a simplifica și organiza procesul birocratic intern.',
     getFreeDemo: 'Obține Demo Gratuit',
+    getYourBussinessOnline: 'Pune-ți afacerea online!',
+    getYourBussinessOnlineDescription: 'Daca ai nevoie de mai multa expunere sau de un website, SmartPart iți este alaturi.',
     metaDescription: 'Obține o analiză și un demo gratuit! Digitalizează-ți afacerea și automatizează procesele repetitive și inutile printr-o aplicatie Smart Part.',
+    planAndAnalyze: 'Analiză și planificare',
+    planAndAnalyzeDescription: 'Realizăm o analiză a proceselor ce se deruleaza in firma dumeanvoastra și propunem posibile solutii de digitalizare a acestora.',
     smartPart: 'Smart Part',
     smartPartCallToAction: 'Smart Part|Digitalizează-ți afacerea!',
     steps: 'Etape',
-    services: 'Servicii'
+    services: 'Servicii',
+    weAreHereToHelp: 'Mentenanță și colaborare',
+    weAreHereToHelpDescription: 'În cazul in care întâmpini defecțiuni tehnice îți suntem alaturi.'
 }
 
 export default LABELS;
