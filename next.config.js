3fa04f020488de71bcb7b1912a2f7dfd796a16fd
Cezar Mocanu <cmocanu@cisco.com>
2021-05-01 13:31:28 +0300

    [master] Fixed lang

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
 
 module.exports = {
+  i18n:{
+    locales: ['ro'],
+    defaultLocale: 'ro',
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
